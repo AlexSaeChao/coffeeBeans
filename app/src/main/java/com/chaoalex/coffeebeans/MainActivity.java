6e5d252e16574bb6c4faf8a3655e1bcb1ce13f2a
AlexSaeChao <chaoalex93@gmail.com>
2023-09-05 20:12:13 -0700

    banner ad implemented, added button for Interstitial

--- app/src/main/java/com/chaoalex/coffeebeans/MainActivity.java
+++ app/src/main/java/com/chaoalex/coffeebeans/MainActivity.java
@@ -4,11 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends AppCompatActivity {
+  private AdRequest bannerAdRequest;
+  private AdView bannerAdView;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    bannerAdView = findViewById(R.id.bannerAdView);
+    initializeMobileAds();
+
+    bannerAdRequest = new AdRequest.Builder().build();
+    bannerAdView.loadAd((bannerAdRequest));
+  }
+
+  private void initializeMobileAds() {
+
+    MobileAds.initialize(this, new OnInitializationCompleteListener() {
+      @Override
+      public void onInitializationComplete(InitializationStatus initializationStatus) {
+      }
+    });
   }
 }
